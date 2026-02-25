a04d92a8b13e88dc449e2e3518a871b488793eac
Manuel <mebaez@ucatolica.edu.co>
2026-02-25 00:21:35 -0500

    commit despues de subirlos github

--- tres.py
+++ tres.py
@@ -1,3 +1,4 @@
 print("Tercer archivover ejemplo tag")
 print("Lo modifico despues de crear la rama y regresar")
 print("modificado por programador")
+print("Modificado despues de suburlo a GitHub")
