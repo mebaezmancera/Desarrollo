852193d258e7be81d0b64af7fa4f2b715ce1e869
Manuel <manbae68@gmail.com>
2026-02-25 01:05:08 -0500

    Update tres.py

--- tres.py
+++ tres.py
@@ -2,3 +2,5 @@ print("Tercer archivover ejemplo tag")
 print("Lo modifico despues de crear la rama y regresar")
 print("modificado por programador")
 print("Modificado despues de suburlo a GitHub")
+print("mod remoto")
+
